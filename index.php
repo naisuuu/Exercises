d57b721b014ea7a089654885372be3b705763a49
Cyril Jacob Mosawan <cjmosawan@yahoo.ie>
2020-10-03 15:29:36 +0100

    Added extra files and code
    new index.php
    added 'for' types in index1.html

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Lesson 6: PHP</title>
+</head>
+<body>	
+	<!--?php
+	
+		//$myString = "This is a string";
+		
+		//echo $myString;
+		
+		if(4<5){
+			echo"Hello,4<5";
+		}
+	?-->
+	<form action="process.php" method="POST">
+		<input type="text" name="username">
+		<input type="password" name="password">
+		<button type="submit">Login</button>
+	</form>
+</body>
+</html>
