deb2401a87ccb0578a3376b73df823c1d4fd528f
Cyril Jacob Mosawan <cjmosawan@yahoo.ie>
2020-10-03 14:01:58 +0100

    First test

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Lesson 6: PHP</title>
+</head>
+<body>	
+	<?php
+	
+		//$myString = "This is a string";
+		
+		//echo $myString;
+		
+		if(4<5){
+			echo"Hello,4<5";
+		}
+	?>
+	
+</body>
+</html>
