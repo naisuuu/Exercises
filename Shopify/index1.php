af5db8cc1cfb1707699b51cac4fc7ff76502c915
Cyril Jacob Mosawan <cjmosawan@yahoo.ie>
2020-10-11 15:07:02 +0100

    Inserted Shopify folder

---
+++ Shopify/index1.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title> Shopify </title>
+		<!-- CSS -->
+		<!-- BootStrap -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" 
+		integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" 
+		crossorigin="anonymous">
+	
+		<!-- FontAwesome -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+		
+		<!-- Custom CSS -->
+		<link rel="stylesheet" href="style.css">
+	</head>
+<body>
+	
+	<!-- Header -->
+	<header>
+	</header>
+	<nav class='navbar navbar-inverse'>
+		<div class='container-fluid'>
+			<header class='navbar-header'>
+				<button type='button' class='navbar-toggle' data-toggle='collapse' data-target='#'>
+				</button>
+			</header>
+		
+		</div>
+	</nav>
+	<!-- Main -->
+	<main>
+	</main>
+	<!-- Footer -->
+	<footer>
+	</footer>
+	<!-- JavaScript -->
+	
+	<!-- JQuery -->
+	<script src="https://code.jquery.com/jquery-3.5.1.min.js" 
+	integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" 
+	crossorigin="anonymous"></script>
+	
+	<!-- BootStrap JavaScript -->
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" 
+	integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" 
+	crossorigin="anonymous"></script>
+	
+</body>
+
+
+</html>
