f4e4317b0f4fc77af01bd9a307e7aa4b1952bbc6
Cyril Jacob Mosawan <cjmosawan@yahoo.ie>
2020-10-11 16:55:09 +0100

    added carousel and extra rows

--- Shopify/index1.php
+++ Shopify/index1.php
@@ -57,6 +57,50 @@
 	</nav>
 	<!-- Main -->
 	<main>
+		<div class='container'>
+			<div class='row'>
+				<div class='col-md-3'>
+					<p class='lead'>Product Range</p>
+					<p>Computers<p>
+					<div class='list-group'>
+						<a href='#' class='list-group-item'>Category 1</a>
+						<a href='#' class='list-group-item'>Category 2</a>
+						<a href='#' class='list-group-item'>Category 3</a>
+					</div>
+					<p>Cars<p>
+					<div class='list-group'>
+						<a href='#' class='list-group-item'>Category 1</a>
+						<a href='#' class='list-group-item'>Category 2</a>
+						<a href='#' class='list-group-item'>Category 3</a>
+					</div>
+				</div>
+				<div class='col-md-9'>
+					<div class='row-carousel-holder'>
+						<div class='col-md-12'>
+							<div id='carousel-example-generic' class='carousel-slide' data-ride='carousel'>
+								<ol class='carousel-indicators'>
+									<li data-target='carousel-example-generic' data-slide-to='0' class='active'></li>
+									<li data-target='carousel-example-generic' data-slide-to='1' ></li>
+									<li data-target='carousel-example-generic' data-slide-to='2' ></li>
+								</ol>
+								<div class='carousel-inner'>
+									<div class='item active'>
+										<img class='slide-image' src='images/ds1.jpg' alt=''>
+									</div>
+									<div class='item'>
+										<img class='slide-image' src='images/ds2.jpg' alt=''>
+									</div>
+									<div class='item'>
+										<img class='slide-image' src='images/ds3.jpg' alt=''>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				</div>
+			</div>
+		</div>
 	</main>
 	<!-- Footer -->
 	<footer class='text-center'>
